2f405f04b8734e67ac67231d8f7ee87236ae4e5c
shenshuxin01 <ssx17852015601@163.com>
2021-07-25 16:54:35 +0800

    dist目录路径错误修复

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,18 @@ module.exports = {
         //
         //     }
         // }
+    },
+    publicPath: './',
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views',
+                'plugins': '@/plugins',
+            }
+        }
     }
 };
